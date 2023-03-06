8149dffd1485223480438e9d34f7e80eb50bce90
ninypops <ninypopsgame@gmail.com>
2023-03-06 19:24:26 +0000

    Install bootstrap and create blank canvas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="ninypops--wrapper">
+      <header className="ninypops--header">
+        <h1>nina</h1>
+        <p>front-end developer using React.</p>
+        <p>I lean towards projects that are centred around people and are socially conscientious. If we're not using technology to make this world a little better, what's the point? I’m a strong advocate for inclusivity and supporting people’s well-being in the workplace.</p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
     </div>
   );
